146aa5ef509a305cbc1edba4fda6014a8e5b564c
david <david@lesicnik.eu>
2026-05-03 17:55:24 +0200

    Fixes for PWA icon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,13 +14,18 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#4f8a5b",
     icons: [
       {
-        src: "/icon",
-        sizes: "any",
+        src: "/icons/icon-192.png",
+        sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon",
-        sizes: "any",
+        src: "/icons/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/icons/icon-maskable-512.png",
+        sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
       },
